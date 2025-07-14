9123f3915cb81506181f7843b507a59445ea898f
Rafa-progz <rafa.trabajo@outlook.es>
2025-07-14 19:37:05 +0000

    falta boton

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,26 +2,42 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const [inputValue, setInputValue] = useState("");
+	const [tareas, setTareas] = useState([]);
+
+	const onInputChange = (e) => {
+		setInputValue(e.target.value);
+	}
+
+	const handleKeyUp = (e) => {
+		if (e.key == "Enter" && inputValue.trim() !== "") {
+			setTareas([...tareas, inputValue]);
+			setInputValue("");
+		}
+	}
+
+
+	return (
+		<>
+			<div className="container">
+				<header><h1>Todas las tareas</h1></header>
+				<label className="form-label" htmlFor="name" >Ecribe una tarea</label>
+				<input className="form-control" id="name" type="text" value={inputValue} onChange={onInputChange} onKeyUp={handleKeyUp} />
+				<div>
+					{tareas.map((tarea, index) => (
+						<ul className="" key={index}>
+							<hr />{tarea}
+							<button className="boton">X</button>
+						</ul>
+				))}
+				</div>
+			</div>
+		</>
 	);
 };
 
